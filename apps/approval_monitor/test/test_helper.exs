16c4573b37c4e4ff7441de245b4a3eea65fe9382
trabianmatt <matt@trabian.com>
2018-04-13 07:21:23 -0400

    Starting over with node

--- apps/approval_monitor/test/test_helper.exs
+++
@@ -1,4 +0,0 @@
-ExUnit.start
-
-[:bypass, :git_hub]
-|> Enum.map(&Application.ensure_all_started/1)
--- apps/git_hub/test/test_helper.exs
+++
@@ -1,4 +0,0 @@
-ExUnit.start()
-
-[:bypass, :logger, :httpoison]
-|> Enum.each(&Application.ensure_all_started/1)
